d4d6b14f67cc8323a2e8a5bb6575ef8f2c3c037f
rikoroku <v2gats213@gmail.com>
2018-01-23 16:18:33 +0900

    Add defense proxy sample.

---
+++ proxy/defense_proxy.rb
@@ -0,0 +1,53 @@
+# Subject
+class BankAccount
+  attr_reader :balance
+
+  def initialize balance
+    @balance = balance
+  end
+
+  def deposit amount
+    @balance += amount
+  end
+
+  def withdraw amount
+    @balance -= amount
+  end
+end
+
+require 'etc'
+
+# Proxy(ユーザー認証を担当する防御プロキシ)
+class BackAccountProxy
+  def initialize real_object, owner_name
+    @real_object = real_object
+    @owner_name = owner_name
+  end
+
+  def balance
+    check_access
+    @real_object.balance
+  end
+
+  def deposit amount
+    check_access
+    @real_object.deposit amount
+  end
+
+  def withdraw amount
+    check_access
+    @real_object.withdraw amount
+  end
+
+  def check_access
+    if Etc.getlogin != @owner_name
+      raise "Illegal access: #{@owner_name} cannot access account."
+    end
+  end
+
+end
+
+account = BankAccount.new 100
+proxy = BackAccountProxy.new account, `logname`
+puts proxy.deposit 60
+puts proxy.withdraw 10
